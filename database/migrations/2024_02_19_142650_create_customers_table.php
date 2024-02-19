ba74683242a8e8049e924d0f8f2dc33fb2dbdeec
krutyosila <ali@soyturk.com>
2024-02-19 22:34:44 +0300

    email

--- database/migrations/2024_02_19_142650_create_customers_table.php
+++ database/migrations/2024_02_19_142650_create_customers_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
             $table->string('phone')->unique();
             $table->bigInteger('identify')->nullable();
             $table->date('birthday')->nullable();
